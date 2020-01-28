7dfb6fbcf0da6af8a175211dd922a3a9b833a401
dukegreene <dukegreene@gmail.com>
2020-01-28 17:00:08 -0600

    finish all restful actions in sinatra

--- sinatra-forms-rest/README.md
+++ sinatra-forms-rest/README.md
@@ -5,3 +5,21 @@ Objectives
 Understand what REST is
 Implement REST conventions when naming routes and resources (and also organizing them)
 Get more practice working with user input via the `params` hash
+**Work with params from multiple sources at once!**
+
+Q&A
+Table fighting over post vs get
+
+get '/kittens' do
+  # kitten index
+end
+
+post '/kittens' do
+  # kitten creation
+end
+
+VERB          WHAT IT DOES
+get
+post
+put/patch
+delete
--- sinatra-forms-rest/app/controllers/books_controller.rb
+++ sinatra-forms-rest/app/controllers/books_controller.rb
@@ -10,19 +10,45 @@ class BooksController < ApplicationController
     erb :'books/new'
   end
 
-  # Show
-  get '/books/:id' do
-    p params
-    @book = Book.find(params[:id])
-    erb :'books/show'
-  end
-
   # Create
   post '/books' do
     p params
+    p nonexistent_var
     # book = Book.create({title: params[:title], author: params[:author], snippet: params[:snippet]})
     book = Book.create(params[:book])
     redirect "/books/#{book.id}"
   end
 
+  # Show
+  get '/books/:id' do
+    p params
+    @book = find_book
+    erb :'books/show'
+  end
+
+  # Edit
+  get '/books/:id/edit' do
+    @book = find_book
+    erb :'books/edit'
+  end
+
+  # Put
+  put '/books/:id' do
+    p params
+    book = find_book
+    p "******************************"
+    book.update(params[:book])
+    redirect "/books/#{book.id}"
+  end
+
+  # Delete
+  delete '/books/:id' do
+    book = find_book
+    book.destroy
+    redirect "/books"
+  end
+
+  def find_book
+    Book.find(params[:id])
+  end
 end
